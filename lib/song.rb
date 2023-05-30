410594a180aa37d2e807166dcdd4b5fb917d960f
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-30 12:55:48 +0300

    Assignment Done

--- lib/song.rb
+++ lib/song.rb
@@ -6,5 +6,29 @@ class Song
     @name = name
     @album = album
   end
+  
+# creating a table
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+
+# # adding a new song to the database
+    def save
+      sql = <<-SQL
+        INSERT INTO songs (name, album)
+        VALUES (?, ?)
+      SQL
+  
+      DB[:conn].execute(sql, self.name, self.album)
+  
+    end
+  end
+
 
 end
